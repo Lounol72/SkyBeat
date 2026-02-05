38d4c5d9584818b54e05bfe7212dc1f06ecac1f4
Pasquier Nathan <nathan_pasquier.etu@univ-lemans.fr>
2026-02-05 16:14:19 +0100

    ajout du composant pour la requête API météo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { GenerateComponent } from './components/generate/generate.component';
+import { Meteo } from './component/meteo/meteo'
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'generate', component: GenerateComponent },
+
+    { path: '', component: Meteo }
 ];
---
+++ src/app/component/meteo/meteo.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-meteo',
+  templateUrl: './meteo.html',
+  imports: [CommonModule],
+  styleUrl: './meteo.css',
+  standalone: true
+})
+export class Meteo implements OnInit {
+
+  temperature?: number;
+
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    // DECOMMENTEZ POUR UTILISER L'API METEO
+
+    /*if (isPlatformBrowser(this.platformId)) {
+      this.getTemperature();
+    }*/
+  }
+
+  getTemperature(): void {
+    const url =
+      'https://api.open-meteo.com/v1/forecast' +
+      '?latitude=48' +
+      '&longitude=0.2' +
+      '&current=temperature_2m' +
+      '&timezone=auto';
+
+    this.http.get<any>(url).subscribe(response => {
+      this.temperature = response.current.temperature_2m;
+      console.log("température reçu : " + response.current.temperature_2m);
+      this.cdr.detectChanges();
+    });
+  }
+}
